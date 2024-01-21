ffb764dc70bdae360fb8973474833e4b838a7548
Ekaums <ekaum.singh@gmail.com>
2024-01-21 11:57:25 -0500

    Better user input + makefile

---
+++ Makefile
@@ -0,0 +1,26 @@
+# Compiler and flags
+CC = gcc
+CFLAGS = -Wall
+
+# Executable name
+TARGET = seashell.out
+
+# Source files
+SRCS = main.c
+
+# Object files
+OBJS = $(SRCS:.c=.o)
+
+# Compile and link
+$(TARGET): $(OBJS)
+	$(CC) $(CFLAGS) -o $(TARGET) $(OBJS)
+
+# Compile source files to object files
+%.o: %.c
+	$(CC) $(CFLAGS) -c $< -o $@
+
+.PHONY: clean
+
+# Clean up
+clean:
+	rm -f $(TARGET) $(OBJS)
--- main.c
+++ main.c
@@ -1,15 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int main(void){
-    char input[256]; // Currently, only 256 chars can be stored
+    char *buffer = NULL;
+    char *midbuffer = NULL;
+    char *token = NULL;
+    size_t size = 0;
 
     do{
         printf("sea> ");
 
-        if (fgets(input, sizeof(input), stdin) == NULL)
-            return 1;
+        ssize_t input = getline(&buffer, &size, stdin); // Get variable sized input from user
 
-    }while(!(strcmp(input, "exit\n") == 0));
+        if (input == -1){
+            printf("ERROR\n");
+            return 1;
+        }
+
+        // Intermediate buffer to parse tokens, sets to NULL at the end of string
+        // (don't want to modify pointer of original buffer)
+        midbuffer = buffer;
+        while((token = strsep(&midbuffer, " "))){
+            printf("%s", token);
+        }
+
+    }while((strcmp(buffer, "exit\n")));
+    
+    free(buffer);
+    return 0;
 
 }
