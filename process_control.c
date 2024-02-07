14027be7204d0099fc2316cf23585e6680d747de
Ekaums <ekaum.singh@gmail.com>
2024-02-07 00:38:21 -0500

    Able to run first command: ls

--- process_control.c
+++ process_control.c
@@ -4,9 +4,8 @@
 #include <unistd.h>
 
 void process(char *arg){
-    char *token = NULL;
+    //char *token = NULL;
     int rc = fork();
-
 
     // while((token = strsep(&arg, " "))){
     //     // If this is the last token, remove the newline
@@ -21,19 +20,26 @@ void process(char *arg){
         // Child
         if(strcmp(arg, "ls\n") == 0){
             if(access("/bin/ls", X_OK) == 0){
+                char *args[2];
+                // Don't want to insert a string literal into array since it is immutable. 
+                // The pointer would point to read only section, which means you cant change args in here.
+                args[0] = strdup("/bin/ls"); 
+                args[1] = NULL;
+                execv(args[0], args);
                 printf("Success\n");
-                exit(0);
             }
+            else{   printf("Error: ls is not a valid command");   }
+            exit(0);
         }
         else{
-            fprintf(stderr, "Error: File does not exist / invalid permission\n");
+            fprintf(stderr, "Error: Invalid command\n");
             exit(0);
         }
     }
     else{
         // Parent
         int rc_wait = wait(NULL);
-        printf("Parent PID: %d of child PID: %d\n", getpid(), rc_wait);
+        // printf("Parent PID: %d of child PID: %d\n", getpid(), rc_wait);
     }
 
     return;
