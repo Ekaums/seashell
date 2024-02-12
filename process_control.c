d0bb851631eabfaeb07f96614172d0ca25d5186f
Ekaums <ekaum.singh@gmail.com>
2024-02-12 11:43:34 -0500

    Can send any standard /bin/ commands

--- process_control.c
+++ process_control.c
@@ -2,44 +2,56 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <assert.h>
 
 void process(char *arg){
-    //char *token = NULL;
+    char *token = NULL;
     int rc = fork();
-
-    // while((token = strsep(&arg, " "))){
-    //     // If this is the last token, remove the newline
-    //     if(token[strlen(token) - 1] == '\n') token[strlen(token) - 1] = '\0';
-    // }
 
     if (rc < 0){
         fprintf(stderr, "fork failed\n");
         exit(1);
     }
-    else if (rc == 0){
+    else if(rc == 0){
         // Child
-        if(strcmp(arg, "ls\n") == 0){
-            if(access("/bin/ls", X_OK) == 0){
-                char *args[2];
-                // Don't want to insert a string literal into array since it is immutable. 
-                // The pointer would point to read only section, which means you cant change args in here.
-                args[0] = strdup("/bin/ls"); 
-                args[1] = NULL;
-                execv(args[0], args);
-                printf("Success\n");
+        if(arg[strlen(arg) - 1] == '\n') arg[strlen(arg) - 1] = '\0';   // Remove newline
+        size_t index = 0;
+        size_t capacity = 2; // Starts at 2 to have arg + NULL
+
+        char **args = (char**)malloc(sizeof(char *) * capacity); // Cannot use char *args[]. This defines a static-sized array which cannot be malloced
+        if(!args) assert(args);
+
+        // Parse arguments, adding them to args array
+        while((token = strsep(&arg, " "))){
+            // Ensure first command is valid
+            if(index == 0){
+                args[index] = strdup("/bin/"); 
+                strcat(args[index], token);
+
+                if(access(args[0], X_OK) != 0){ 
+                    printf("Invalid Command\n");
+                    exit(-1);
+                }
+                index += 1;
+                continue;
             }
-            else{   printf("Error: ls is not a valid command");   }
-            exit(0);
+            // Double size of array if more args
+            if (index+1 >= capacity){
+                capacity *= 2; 
+                args = realloc(args, capacity * sizeof(char *));
+                if(!args) assert(args);
+            }
+            args[index++] = token;
         }
-        else{
-            fprintf(stderr, "Error: Invalid command\n");
-            exit(0);
-        }
+
+        args[index] = NULL; // Add NULL before calling execv
+        execv(args[0], args);
+
     }
     else{
         // Parent
         int rc_wait = wait(NULL);
-        // printf("Parent PID: %d of child PID: %d\n", getpid(), rc_wait);
+        printf("Done\n");
     }
 
     return;
